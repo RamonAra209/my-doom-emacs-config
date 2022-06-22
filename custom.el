972befcdde69b05430684436cf6b5720573f9757
Ramon Arambula <r_arambula@u.pacifc.edu>
2022-06-21 23:28:06 -0700

    Updated custom.el

--- custom.el
+++ custom.el
@@ -4,8 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.0)
+ '(doom-modeline-height 25)
  '(package-selected-packages
-   '(org-bullets preview-dvisvgm org-fancy-priorities org-super-agenda evil-snipe)))
+   '(impatient-mode org-bullets preview-dvisvgm org-fancy-priorities org-super-agenda evil-snipe)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
